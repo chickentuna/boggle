bcf12556e0c38c65af19fcd7a2fd8793c1c43e15
Julien <julien@codingame.com>
2019-11-19 00:32:18 +0100

    Display a grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import './App.css';
-import Phaser from "phaser";
+import React from 'react'
+import './App.css'
+import Phaser from 'phaser'
 import woodTiles from './assets/Wood.js'
 import availableDie from './dice'
 
 var boardSize = 4
-var state = {
-
-}
-
+var state = {}
 
 const config = {
   type: Phaser.AUTO,
-  parent: "game",
+  parent: 'game',
   width: 800,
   height: 600,
   scene: {
     preload: preload,
     create: create
   }
-};
+}
 
 /**
  * Shuffles an array inplace
  **/
-function shuffleArray(array) {
+function shuffleArray (array) {
   for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
   }
 }
 
@@ -35,7 +32,7 @@ function shuffleArray(array) {
  * Returns an int in [low;high[
  * or [0; high[ if second parameter is left out
  **/
-function randomInt(low, high) {
+function randomInt (low, high) {
   if (high == null) {
     return randomInt(0, low)
   }
@@ -45,26 +42,25 @@ function randomInt(low, high) {
   return Math.floor((Math.random() * (high - low)) + low)
 }
 
-function preload() {
+function preload () {
   for (const letter of 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') {
-    this.load.image(`wood_${letter}`, woodTiles[letter]);
+    this.load.image(`wood_${letter}`, woodTiles[letter])
   }
 }
 
-function create() {
-  const logo = this.add.image(400, 150, "wood_A");
-  // const board = initBoard(boardSize)
-  // initBoardView(board,this)
+function create () {
+  const board = initBoard(boardSize)
+  console.log(board.length, board[0].length)
+  initBoardView(board, this)
 }
 
-function initBoard(size) {
+function initBoard (size) {
   const board = []
   const dieLeft = [...availableDie]
   shuffleArray(dieLeft)
   for (let y = 0; y < size; ++y) {
     const row = []
     for (let x = 0; x < size; ++x) {
-      
       const dice = dieLeft.shift()
       // Roll dice
       const letter = dice[randomInt(dice.length)]
@@ -75,30 +71,35 @@ function initBoard(size) {
   return board
 }
 
+function initBoardView (board, game) {
+  const boardX = 120
+  const boardY = 120
+  const tileSize = 80
 
-function initBoardView(board, game) {
   const view = []
   for (let y = 0; y < board.length; ++y) {
     const row = []
     for (let x = 0; x < board[0].length; ++x) {
       const letter = board[y][x]
-      game.add.image(400, 150, `wood_${letter}`);
+      const tileX = boardX + x * tileSize
+      const tileY = boardY + y * tileSize
+      const img = game.add.image(tileX, tileY, `wood_${letter}`)
+      img.scale = tileSize / 256
+      row.push(img)
     }
-    board.push(row)
+    view.push(row)
   }
-  return board
+  return view
 }
 
-
-function App() {
-
-  const game = new Phaser.Game(config);
+function App () {
+  const game = new Phaser.Game(config)
 
   return (
-    <div className="App">
-      <div id="game"></div>
+    <div className='App'>
+      <div id='game' />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
