dff0c3f80838cdf074bf6a58f94ee4644daf505a
Valentin Vetter <vetter.valentin@gmail.com>
2019-12-01 22:55:32 +0100

    refactor: extract a GameScene

--- src/App.tsx
+++ src/App.tsx
@@ -2,69 +2,14 @@ import React from 'react'
 import Phaser from 'phaser'
 
 import './App.css'
-import woodTiles from './assets/Wood'
-import availableDie from './dice'
+import GameScene from './GameScene'
 
-const boardSize = 4
-
-const config = {
+const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   parent: 'game',
   width: 800,
   height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
-}
-
-function preload () {
-  for (const letter of 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') {
-    this.load.image(`wood_${letter}`, woodTiles[letter])
-  }
-}
-
-function create () {
-  const board = initBoard(boardSize)
-  console.log(board.length, board[0].length)
-  initBoardView(board, this)
-}
-
-function initBoard (size: number): string[][] {
-  const board = []
-  const dieLeft = Phaser.Math.RND.shuffle(availableDie)
-  for (let y = 0; y < size; ++y) {
-    const row = []
-    for (let x = 0; x < size; ++x) {
-      const dice = dieLeft.shift()
-      // Roll dice
-      const letter = Phaser.Math.RND.pick(dice)
-      row.push(letter)
-    }
-    board.push(row)
-  }
-  return board
-}
-
-function initBoardView (board, game) {
-  const boardX = 120
-  const boardY = 120
-  const tileSize = 80
-
-  const view = []
-  for (let y = 0; y < board.length; ++y) {
-    const row = []
-    for (let x = 0; x < board[0].length; ++x) {
-      const letter = board[y][x]
-      const tileX = boardX + x * tileSize
-      const tileY = boardY + y * tileSize
-      const img = game.add.image(tileX, tileY, `wood_${letter}`)
-      img.scale = tileSize / 256
-      row.push(img)
-    }
-    view.push(row)
-  }
-  return view
+  scene: GameScene
 }
 
 function App () {
---
+++ src/GameScene.ts
@@ -0,0 +1,57 @@
+import Phaser from 'phaser'
+
+import woodTiles from './assets/Wood'
+import availableDie from './dice'
+
+const boardSize = 4
+
+export default class GameScene extends Phaser.Scene {
+  preload () {
+    for (const letter of 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+      this.load.image(`wood_${letter}`, woodTiles[letter])
+    }
+  }
+  
+  create () {
+    const board = this.initBoard(boardSize)
+    console.log(board)
+    this.initBoardView(board)
+  }
+  
+  initBoard (size: number): string[][] {
+    const board = []
+    const dieLeft = Phaser.Math.RND.shuffle(availableDie)
+    for (let y = 0; y < size; ++y) {
+      const row = []
+      for (let x = 0; x < size; ++x) {
+        const dice = dieLeft.shift()
+        // Roll dice
+        const letter = Phaser.Math.RND.pick(dice)
+        row.push(letter)
+      }
+      board.push(row)
+    }
+    return board
+  }
+  
+  initBoardView (board: string[][]) {
+    const boardX = 120
+    const boardY = 120
+    const tileSize = 80
+  
+    const view = []
+    for (let y = 0; y < board.length; ++y) {
+      const row = []
+      for (let x = 0; x < board[0].length; ++x) {
+        const letter = board[y][x]
+        const tileX = boardX + x * tileSize
+        const tileY = boardY + y * tileSize
+        const img = this.add.image(tileX, tileY, `wood_${letter}`)
+        img.scale = tileSize / 256
+        row.push(img)
+      }
+      view.push(row)
+    }
+    return view
+  }
+}
